db5a0c18df6bd1538e5d9ea4ae62c5c79046f913
phoebus-84 <ennio@dyne.org>
2023-06-21 17:00:39 +0200

    try without submodules

--- saas/hooks/post_gen_project.py
+++ saas/hooks/post_gen_project.py
@@ -2,6 +2,7 @@ import os
 import shutil
 
 def create_env():
+    print("Creating .env file...")
     base_dir = os.path.join(os.getcwd(), "webapp")
     source_file = os.path.join(base_dir, '.env.example')
     destination_file = os.path.join(base_dir, '.env')
--- saas/hooks/post_gen_project.sh
+++ saas/hooks/post_gen_project.sh
@@ -1,17 +1,21 @@
 #!/bin/env bash
 
+echo starting post gen
+
+
 set -euo pipefail
 
 # cd ../{{cookiecutter.project_name}}/admin/pb_data
 # VALUE=$(sqlite3 data.db 'select value from _params where key like "settings";' | sed -En 's/Acme/{{cookiecutter.project_name}}/p')
 # sqlite3 data.db "update _params set value = '${VALUE}' where key like 'settings'"
 
+
 git init
 git branch -m main
 
-ZENCODE_CRYPTO=admin/zencode/zenflows-crypto
-rmdir $ZENCODE_CRYPTO
-[ ! -d $ZENCODE_CRYPTO ] && git submodule add https://github.com/interfacerproject/zenflows-crypto $ZENCODE_CRYPTO
+# ZENCODE_CRYPTO=admin/zencode/zenflows-crypto
+# rmdir $ZENCODE_CRYPTO
+# [ ! -d $ZENCODE_CRYPTO ] && git submodule add https://github.com/interfacerproject/zenflows-crypto $ZENCODE_CRYPTO
 
 echo 🎉 Everything is done please run
 
